f2223d1f185e846707976f7488d509b51186f9f7
Dmitry Papka <dmitry-papka@github.com>
2014-05-31 16:07:36 +0300

    Test cases updated

--- sorting_test.go
+++ sorting_test.go
@@ -11,9 +11,34 @@ type TestCase struct {
 }
 
 var testCases []TestCase = []TestCase {
+	// Positive numbers
 	TestCase {
-		input: []int {5, -1, 8, 5, 3},
-		expected: []int {-1, 3, 5, 5, 8},
+		input: []int {9, 8, 7, 6, 5, 4, 3, 2, 1},
+		expected: []int {1, 2, 3, 4, 5, 6, 7, 8, 9},
+	},
+	
+	// Negative numbers
+	TestCase {
+		input: []int {-6, -3, -2, -9, -7, -3},
+		expected: []int {-9, -7, -6, -3, -3, -2},
+	},
+	
+	// Positive and negative numbers
+	TestCase {
+		input: []int {5, -1, 8, 0, -2, 3},
+		expected: []int {-2, -1, 0, 3, 5, 8},
+	},
+	
+	// Decreasing sorted slice
+	TestCase {
+		input: []int {5, 4, 3, 2, 1, 0, -1, -2},
+		expected: []int {-2, -1, 0, 1, 2, 3, 4, 5},
+	},
+	
+	// Empty slice
+	TestCase {
+		input: []int {},
+		expected: []int {},
 	},
 }
 
